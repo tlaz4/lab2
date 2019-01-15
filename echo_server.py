9df7e88a37234458aafb734e518b904535ec1b07
Alexander Wong <awwong1@ualberta.ca>
2019-01-15 09:34:26 -0700

    mid way through lab

--- echo_server.py
+++ echo_server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import socket
+import time
 
 HOST = ""
 PORT = 8081
@@ -8,12 +9,16 @@ BUFFER_SIZE = 1024
 def main():
     
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
+
         s.listen(1)
 
         while True:
             conn, addr = s.accept()
             with conn:
+                print(conn)
                 print("Connected by:", addr)
                 full_data = b""
                 while True:
@@ -21,10 +26,9 @@ def main():
                     if not data:
                         break
                     full_data += data
-                print(full_data)
+                # print(full_data)
+                time.sleep(0.5)
                 conn.sendall(full_data)
-
-
 
 if __name__ == "__main__":
     main()
