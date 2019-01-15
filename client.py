398100966a3594f6a0a4902d7aeac7931378dccf
Alexander Wong <awwong1@ualberta.ca>
2019-01-15 08:59:46 -0700

    working tcp get google.com

--- client.py
+++ client.py
@@ -5,15 +5,39 @@ HOST = "www.google.com"
 PORT = 80
 BUFFER_SIZE = 1024
 
+payload = """GET / HTTP/1.0
+Host: {HOST}
+
+""".format(HOST=HOST)
+
+def conn_socket(addr_tup):
+    (family, socktype, proto, canonname, sockaddr) = addr_tup
+    try:
+        s = socket.socket(family, socktype, proto)
+        s.connect(sockaddr)
+        s.sendall(payload.encode())
+        
+        full_data = b""
+        while True:
+            data = s.recv(BUFFER_SIZE)
+            if not data:
+                break
+            full_data += data
+
+        print(full_data)
+    except e:
+        print(e)
+        pass
+    finally:
+        s.close()
+
 def main():
     addr_info = socket.getaddrinfo(HOST, PORT, proto=socket.SOL_TCP)
     #print(addr_info)
     for addr_tup in addr_info:
-        # Destructure the socket returned tuple
-        print(addr_tup)
-        (family, socktype, proto, canonname, sockaddr) = addr_tup
-
-        
+        conn_socket(addr_tup) 
+        # only ipv4 I guess
+        break
 
 if __name__ == "__main__":
     main()
